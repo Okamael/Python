2bfa98c0bbf854aebfcd397ae2a7a9dc6f071328
Okamael <46298240+Okamael@users.noreply.github.com>
2019-09-08 11:17:40 -0300

    Update Cep.py
    Atualização para abrir no navegador a  localização no googlemaps

--- Cep.py
+++ Cep.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import webbrowser
 
 
 inputCep= input('Digite o cep:')
@@ -11,11 +12,24 @@ if len(inputCep) != 8:
 requests = requests.get('https://cep.awesomeapi.com.br/json/{}'.format(inputCep))
 
 retornoCep = requests.json()
-
+#openInBrowserMap= 'https://www.google.com/maps/search/?api=1&query={},{}'.format(input)
+#print(retornoCep)
 print("Cep:{}".format(retornoCep['cep']))
 print("Rua:{}".format(retornoCep['address_name']))
 print("Bairro:{}".format(retornoCep['district']))
 print("Cidade:{}".format(retornoCep['city']))
-print("Cidade:{}".format(retornoCep['state']))
+print("Estado:{}".format(retornoCep['state']))
+print('Latitude:{}'.format(retornoCep['lat']))
+print('Longitude:{}'.format(retornoCep['lng']))
+
+Latitude='{}'.format(retornoCep['lat'])
+Longitude='{}'.format(retornoCep['lng'])
+
+openInBrowserMap= 'https://www.google.com/maps/search/?api=1&query='+Latitude+','+Longitude+''
+
+webbrowser.open_new(openInBrowserMap)
+
+
+
 
 
